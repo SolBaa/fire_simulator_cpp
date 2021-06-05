f12e69c8e5951e19bf9d005d923f61694e74b6e3
Sol Battaglia <solbattaglia@gmail.com>
2021-06-05 12:36:00 -0300

    creating a SDL window

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# fire_simulator_cpp
+# Particle Fire Explosion Simulator
+
+## This is an App using C++ and SDL2
+
+- For run this project:
+1) Download project 
+
+`git clone https://github.com/SolBaa/fire_simulator_cpp.git`
+
+2) Run the project
+
+` g++ main.cpp -o main -lSDL2 -lSDL2_mixer -lSDL2_image -lSDL2_ttf && ./main`
+
+3) Enjoy!!
--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,48 @@
 
 #include <SDL2/SDL.h>
 #include <iostream>
+#include <stdio.h>
 
 using namespace std;
 
 
 int main(){
+	const int SCREEN_WIDTH = 1000;
+	const int SCREEN_HEIGHT = 800;
+
 	if(SDL_Init(SDL_INIT_VIDEO)<0){
 		cout<<"SDL Failded!!"<<endl;
 
 		return false;
 	}
+	// Initialize Windows
+	SDL_Window *window = SDL_CreateWindow("Particle Fire Explosion", 
+	SDL_WINDOWPOS_UNDEFINED,SDL_WINDOWPOS_UNDEFINED,SCREEN_WIDTH,SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
 
-	cout<<"SDL Succeeded!!"<<endl;
+	if(window==NULL){
+		printf("Could not create window: %s\n", SDL_GetError());
+		SDL_Quit();
+		return 2;
+	}
+
+	bool quit = false;
+	SDL_Event event;
+
+	while(!quit){
+		// Update Particles
+		//Draw Particles.
+		//Check for messsages/events
+
+		while(SDL_PollEvent(&event)){
+			if(event.type==SDL_QUIT){
+				quit = true;
+			}
+
+		}
+	}
+
+	//After the program finished we need to destroy the window
+	SDL_DestroyWindow(window);
     SDL_Quit();
 
 	return 0;
