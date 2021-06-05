de932a6154a4c3049bc143a7dadab2f9f4879b84
Sol Battaglia <solbattaglia@gmail.com>
2021-06-05 13:13:11 -0300

    Add renderer and textures

--- src/main.cpp
+++ src/main.cpp
@@ -25,6 +25,35 @@ int main(){
 		return 2;
 	}
 
+	SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_PRESENTVSYNC);
+	SDL_Texture *texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGBA8888,SDL_TEXTUREACCESS_STATIC, SCREEN_WIDTH, SCREEN_HEIGHT);
+
+
+	if(renderer == NULL){
+		cout<<"Couldn't create renderer"<<endl;
+		SDL_DestroyWindow(window);
+
+		SDL_Quit();
+
+		return 2;
+	}
+
+	if(texture == NULL){
+		cout<<"Couldn't create texture"<<endl;
+		SDL_DestroyRenderer(renderer);
+
+		SDL_Quit();
+
+	return 2;
+	}
+
+	Uint32 *buffer = new Uint32[SCREEN_WIDTH*SCREEN_HEIGHT];
+	memset(buffer, 255, SCREEN_WIDTH*SCREEN_HEIGHT*sizeof(Uint32));
+
+	SDL_UpdateTexture(texture, NULL, buffer,SCREEN_WIDTH*sizeof(Uint32));
+	SDL_RenderClear(renderer);
+	SDL_RenderCopy(renderer,texture,NULL, NULL);
+	SDL_RenderPresent(renderer);
 	bool quit = false;
 	SDL_Event event;
 
@@ -42,6 +71,9 @@ int main(){
 	}
 
 	//After the program finished we need to destroy the window
+	delete [] buffer;
+	SDL_DestroyRenderer(renderer);
+	SDL_DestroyTexture(texture);
 	SDL_DestroyWindow(window);
     SDL_Quit();
 
